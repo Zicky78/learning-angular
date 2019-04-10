058b6a19bdac0f008f2bc1f2428bb484b7bd9709
Zicky78 <16zns01@gmail.com>
2019-04-09 17:14:49 -0700

    assignment 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,24 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created';
-  serverName = 'Test Server';
+  username = 'username';
+  allowClearUsername = true;
   constructor() {
-    setTimeout(() => {
-      this.allowNewServer = true;
-    },2000)
+    
    }
 
   ngOnInit() {
   }
 
-  onCreateServer() {
-    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  onClearUsername() {
+    this.username = '';
+    this.allowClearUsername = false;
   }
-
-  onUpdateServerName(event:Event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
+  checkString() {
+    this.allowClearUsername = this.username != '';
   }
-
+  
 }
