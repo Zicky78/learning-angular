cab44271429d1bfb3d070f79735a70b364c41162
Zicky78 <16zns01@gmail.com>
2019-04-17 16:16:07 -0700

    idk

--- src/app/a3/a3.component.ts
+++ src/app/a3/a3.component.ts
@@ -4,11 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-a3',
   templateUrl: './a3.component.html',
   styleUrls: ['./a3.component.css'],
-  styles: [`
-        .white {
-            color: 'white';
-        }
-    `]
+  
 })
 export class A3Component implements OnInit {
 
